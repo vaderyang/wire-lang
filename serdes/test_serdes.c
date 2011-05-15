db2ddfcbc989c1531a0fb31ac7ac10e0a40e8e2f
bladyjoker <bladyjoker@e2c90134-8e21-952c-3922-7c01991208db>
2011-05-15 19:22:31 +0000

    Fixed some scary little bugzis.

--- serdes/test_serdes.c
+++ serdes/test_serdes.c
@@ -4,24 +4,33 @@
 int main(){
 	pserdes_t psd_push = serdes_new();
 	
+	for(int i=20; i<33; i++){
+		serdes_push_uint(psd_push, 0x12345, i, ORD_NONE, ORD_BE);
+	}
+	for(int i=20; i<33; i++){
+		serdes_push_uint(psd_push, 0x12345, i, ORD_NONE, ORD_LE);
+	}
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_LE, ORD_BE);
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_BE, ORD_BE);
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_LE, ORD_LE);
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_BE, ORD_LE);
 
-	serdes_push_uint(psd_push, 0x12345, 21, ORD_NONE, ORD_LE);
-	serdes_push_uint(psd_push, 0x12345, 21, ORD_NONE, ORD_BE);
 	
 	pserdes_t psd_pull = serdes_new();
 	serdes_set(psd_pull, psd_push->pbs, 0);
 
+	for(int i=20; i<33; i++){
+		fprintf(stdout, "%d -> 0x%.8x\n", i, serdes_pull_uint(psd_pull, i, ORD_NONE, ORD_BE));
+	}
+	
+	for(int i=20; i<33; i++){
+		fprintf(stdout, "%d -> 0x%.8x\n", i, serdes_pull_uint(psd_pull, i, ORD_NONE, ORD_LE));
+	}
+	
 	fprintf(stdout, "1: 0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_BE));
 	fprintf(stdout, "2: 0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_BE));
 	fprintf(stdout, "3: 0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_LE));
 	fprintf(stdout, "4: 0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_LE));
-	
-	fprintf(stdout, "5: 0x%.8x\n", serdes_pull_uint(psd_pull, 21, ORD_NONE, ORD_LE));
-	fprintf(stdout, "6: 0x%.8x\n", serdes_pull_uint(psd_pull, 21, ORD_NONE, ORD_BE));
 
 	return 0;
 }
