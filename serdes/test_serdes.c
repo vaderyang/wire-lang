aa41c59b04760850f8b41e749b0e2a5bc120d842
bladyjoker <bladyjoker@e2c90134-8e21-952c-3922-7c01991208db>
2011-05-15 23:39:55 +0000

    SINT for LE machines. Code polish.

--- serdes/test_serdes.c
+++ serdes/test_serdes.c
@@ -10,6 +10,9 @@ int main(){
 	for(int i=20; i<33; i++){
 		serdes_push_uint(psd_push, 0x12345, i, ORD_NONE, ORD_LE);
 	}
+	
+	serdes_push_sint(psd_push, -2, 13, ORD_NONE, ORD_LE);
+	
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_LE, ORD_BE);
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_BE, ORD_BE);
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_LE, ORD_LE);
@@ -27,10 +30,12 @@ int main(){
 		fprintf(stdout, "%d -> 0x%.8x\n", i, serdes_pull_uint(psd_pull, i, ORD_NONE, ORD_LE));
 	}
 	
-	fprintf(stdout, "1: 0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_BE));
-	fprintf(stdout, "2: 0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_BE));
-	fprintf(stdout, "3: 0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_LE));
-	fprintf(stdout, "4: 0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_LE));
+	fprintf(stdout, "%d\n", serdes_pull_sint(psd_pull, 13, ORD_NONE, ORD_LE));
+	
+	fprintf(stdout, "0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_BE));
+	fprintf(stdout, "0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_BE));
+	fprintf(stdout, "0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_LE));
+	fprintf(stdout, "0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_LE));
 
 	return 0;
 }
