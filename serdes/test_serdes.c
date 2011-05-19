e8b0b5adba4e1962e725736066a76925def2444f
bladyjoker <bladyjoker@e2c90134-8e21-952c-3922-7c01991208db>
2011-05-19 11:32:43 +0000

    Small changes on serdes testing.

--- serdes/test_serdes.c
+++ serdes/test_serdes.c
@@ -3,16 +3,16 @@
 
 int main(){
 	pserdes_t psd_push = serdes_new();
-	
+
 	for(int i=20; i<33; i++){
 		serdes_push_uint(psd_push, 0x12345, i, ORD_NONE, ORD_BE);
 	}
 	for(int i=20; i<33; i++){
 		serdes_push_uint(psd_push, 0x12345, i, ORD_NONE, ORD_LE);
 	}
-	
+
 	serdes_push_sint(psd_push, -2, 13, ORD_NONE, ORD_LE);
-	
+
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_LE, ORD_BE);
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_BE, ORD_BE);
 	serdes_push_uint(psd_push, 0x12345678, SZ_LONG, ORD_LE, ORD_LE);
@@ -22,39 +22,41 @@ int main(){
 	serdes_push_fp(psd_push, 3.14, SZ_LONG, FP_IEEE754, ORD_BE, ORD_BE);
 	serdes_push_fp(psd_push, 3.14, SZ_LONG, FP_IEEE754, ORD_LE, ORD_LE);
 	serdes_push_fp(psd_push, 3.14, SZ_LONG, FP_IEEE754, ORD_BE, ORD_LE);
-	
+
 	serdes_push_fp(psd_push, 3.14, SZ_LONG*2, FP_IEEE754, ORD_LE, ORD_BE);
 	serdes_push_fp(psd_push, 3.14, SZ_LONG*2, FP_IEEE754, ORD_BE, ORD_BE);
 	serdes_push_fp(psd_push, 3.14, SZ_LONG*2, FP_IEEE754, ORD_LE, ORD_LE);
 	serdes_push_fp(psd_push, 3.14, SZ_LONG*2, FP_IEEE754, ORD_BE, ORD_LE);
-	
+
 	pserdes_t psd_pull = serdes_new();
 	serdes_set(psd_pull, psd_push->pbs, 0);
 
 	for(int i=20; i<33; i++){
-		fprintf(stdout, "%d -> 0x%.8x\n", i, serdes_pull_uint(psd_pull, i, ORD_NONE, ORD_BE));
+		fprintf(stdout, "%d -> 0x%.8x\n", i, (int)serdes_pull_uint(psd_pull, i, ORD_NONE, ORD_BE));
 	}
-	
+
 	for(int i=20; i<33; i++){
-		fprintf(stdout, "%d -> 0x%.8x\n", i, serdes_pull_uint(psd_pull, i, ORD_NONE, ORD_LE));
+		fprintf(stdout, "%d -> 0x%.8x\n", i, (int)serdes_pull_uint(psd_pull, i, ORD_NONE, ORD_LE));
 	}
-	
-	fprintf(stdout, "%d\n", serdes_pull_sint(psd_pull, 13, ORD_NONE, ORD_LE));
-	
-	fprintf(stdout, "0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_BE));
-	fprintf(stdout, "0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_BE));
-	fprintf(stdout, "0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_LE));
-	fprintf(stdout, "0x%.8x\n", serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_LE));
 
-	fprintf(stdout, "%f\n", serdes_pull_fp(psd_pull, SZ_LONG, FP_IEEE754, ORD_LE, ORD_BE));
-	fprintf(stdout, "%f\n", serdes_pull_fp(psd_pull, SZ_LONG, FP_IEEE754, ORD_BE, ORD_BE));
-	fprintf(stdout, "%f\n", serdes_pull_fp(psd_pull, SZ_LONG, FP_IEEE754, ORD_LE, ORD_LE));
-	fprintf(stdout, "%f\n", serdes_pull_fp(psd_pull, SZ_LONG, FP_IEEE754, ORD_BE, ORD_LE));
+	fprintf(stdout, "%d\n", (int)serdes_pull_sint(psd_pull, 13, ORD_NONE, ORD_LE));
 
-	fprintf(stdout, "%f\n", serdes_pull_fp(psd_pull, SZ_LONG*2, FP_IEEE754, ORD_LE, ORD_BE));
-	fprintf(stdout, "%f\n", serdes_pull_fp(psd_pull, SZ_LONG*2, FP_IEEE754, ORD_BE, ORD_BE));
-	fprintf(stdout, "%f\n", serdes_pull_fp(psd_pull, SZ_LONG*2, FP_IEEE754, ORD_LE, ORD_LE));
-	fprintf(stdout, "%f\n", serdes_pull_fp(psd_pull, SZ_LONG*2, FP_IEEE754, ORD_BE, ORD_LE));
-	
+	fprintf(stdout, "0x%.8x\n", (int)serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_BE));
+	fprintf(stdout, "0x%.8x\n", (int)serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_BE));
+	fprintf(stdout, "0x%.8x\n", (int)serdes_pull_uint(psd_pull, SZ_LONG, ORD_LE, ORD_LE));
+	fprintf(stdout, "0x%.8x\n", (int)serdes_pull_uint(psd_pull, SZ_LONG, ORD_BE, ORD_LE));
+
+	fprintf(stdout, "%f\n", (float)serdes_pull_fp(psd_pull, SZ_LONG, FP_IEEE754, ORD_LE, ORD_BE));
+	fprintf(stdout, "%f\n", (float)serdes_pull_fp(psd_pull, SZ_LONG, FP_IEEE754, ORD_BE, ORD_BE));
+	fprintf(stdout, "%f\n", (float)serdes_pull_fp(psd_pull, SZ_LONG, FP_IEEE754, ORD_LE, ORD_LE));
+	fprintf(stdout, "%f\n", (float)serdes_pull_fp(psd_pull, SZ_LONG, FP_IEEE754, ORD_BE, ORD_LE));
+
+	fprintf(stdout, "%f\n", (float)serdes_pull_fp(psd_pull, SZ_LONG*2, FP_IEEE754, ORD_LE, ORD_BE));
+	fprintf(stdout, "%f\n", (float)serdes_pull_fp(psd_pull, SZ_LONG*2, FP_IEEE754, ORD_BE, ORD_BE));
+	fprintf(stdout, "%f\n", (float)serdes_pull_fp(psd_pull, SZ_LONG*2, FP_IEEE754, ORD_LE, ORD_LE));
+	fprintf(stdout, "%f\n", (float)serdes_pull_fp(psd_pull, SZ_LONG*2, FP_IEEE754, ORD_BE, ORD_LE));
+
+    serdes_del(psd_push);
+    serdes_del(psd_pull);
 	return 0;
 }
