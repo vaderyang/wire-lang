9e6b9bf661acb67c7c42c8e1557f6e9f0e632d2e
bladyjoker <bladyjoker@e2c90134-8e21-952c-3922-7c01991208db>
2011-07-23 01:58:41 +0000

    Replaced macro defs with enumerations.

--- bitstring/bitstring.h
+++ bitstring/bitstring.h
@@ -22,9 +22,12 @@
 @desc Byte order enum.
 @note ORD_NONE is for bit oriented serialization (rather than byte oriented).
 */
-#define ORD_BE 1
-#define ORD_LE 0
-#define ORD_NONE 2
+enum order{
+	ORD_BE,
+	ORD_LE,
+	ORD_NONE
+};
+typedef enum order order_t;
 
 /*
 @desc Native byte/bit ordering macros.
@@ -35,10 +38,13 @@
 /*
 @desc Floating point representation enum.
 */
-#define FP_IEEE754 0
-#define FP_IBM 1
-#define FP_CRAY 2
-#define FP_VAX 3
+enum fprep{
+	FP_IEEE754,
+	FP_IBM,
+	FP_CRAY,
+	FP_VAX
+};
+typedef enum fprep fprep_t;
 
 /*
 @desc Type definitions for bitstring input values.
@@ -71,23 +77,23 @@ pbitstring_t bitstring_concat(pbitstring_t src1, pbitstring_t src2);
 void bitstring_print(FILE *fp, pbitstring_t pbs);
 pbitstring_t bitstring_set(pbitstring_t pbs, void* data, unsigned int size);
 pbitstring_t bitstring_copy(pbitstring_t pbs_src);
-pbitstring_t bitstring_new_uint(uint_t val, unsigned int size, char byte_order, char bit_order);
-pbitstring_t bitstring_new_sint(sint_t val, unsigned int size, char byte_order, char bit_order);
-pbitstring_t bitstring_new_fp(fp_t val, unsigned int size, char fp_rep, char byte_order, char bit_order);
+pbitstring_t bitstring_new_uint(uint_t val, unsigned int size, order_t byte_order, order_t bit_order);
+pbitstring_t bitstring_new_sint(sint_t val, unsigned int size, order_t byte_order, order_t bit_order);
+pbitstring_t bitstring_new_fp(fp_t val, unsigned int size, fprep_t fp_rep, order_t byte_order, order_t bit_order);
 
 /*
 @desc Basic operations.
 */
-void conv_byte_order(void* pval, unsigned int size_bytes, char byte_order);
-void conv_bit_order(char* pval, char bit_order);
-void conv_byte_bit_order(char* pval, unsigned int size_bytes, char byte_order, char bit_order);
+void conv_byte_order(void* pval, unsigned int size_bytes, order_t byte_order);
+void conv_bit_order(char* pval, order_t bit_order);
+void conv_byte_bit_order(char* pval, unsigned int size_bytes, order_t byte_order, order_t bit_order);
 void shift_right_le(char* pval, unsigned int size_bytes, unsigned int shift_count);
 void shift_left_le(char* pval, unsigned int size_bytes, unsigned int shift_count);
 void shift_left_be(char* pval, unsigned int size_bytes, unsigned int shift_count);
 void shift_left(char* pval, unsigned int size_bytes, unsigned int shift_count);
-char get_native_byte_order();
-char get_native_bit_order();
+order_t get_native_byte_order();
+order_t get_native_bit_order();
 unsigned int get_native_long_int_sz();
-char get_native_fp_rep();
+fprep_t get_native_fp_rep();
 
 #endif //BSTR_H
